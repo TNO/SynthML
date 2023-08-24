b3340de646dee0e6f04bc0290459ba24573c49b3
Nan Yang <nanyang1993wendy@gmail.com>
2023-08-24 15:18:45 +0200

    Address Dennis' comments (a bug that can be triggered if there multiple incoming and outgoing edges at the boudaries of actvity diagrams)

--- plugins/com.github.tno.pokayoke.transform.common/src/com/github/tno/pokayoke/transform/common/FlattenUMLActivityDiagram.java
+++ plugins/com.github.tno.pokayoke.transform.common/src/com/github/tno/pokayoke/transform/common/FlattenUMLActivityDiagram.java
@@ -2,7 +2,7 @@
 package com.github.tno.pokayoke.transform.common;
 
 import java.io.IOException;
-import java.util.LinkedHashSet;
+import java.util.ArrayList;
 
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.uml2.uml.Activity;
@@ -12,8 +12,11 @@ import org.eclipse.uml2.uml.ActivityNode;
 import org.eclipse.uml2.uml.Behavior;
 import org.eclipse.uml2.uml.CallBehaviorAction;
 import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.ControlFlow;
 import org.eclipse.uml2.uml.InitialNode;
 import org.eclipse.uml2.uml.Model;
+
+import com.google.common.base.Verify;
 
 /** Flatten nested UML activity diagrams. */
 public class FlattenUMLActivityDiagram {
@@ -33,9 +36,10 @@ public class FlattenUMLActivityDiagram {
         // Extract activities.
         Class contextClass = (Class)model.getMember("Context");
         // Transform all activity behaviors of 'contextClass'.
-        for (Behavior behavior: new LinkedHashSet<>(contextClass.getOwnedBehaviors())) {
-            if (behavior instanceof Activity) {
-                flattenActivityDiagram((Activity)behavior, null);
+        for (Behavior behavior: new ArrayList<>(contextClass.getOwnedBehaviors())) {
+            if (behavior instanceof Activity behaviorActivity) {
+                flattenActivityDiagram(behaviorActivity, null);
+                UMLActivityUtils.removeIrrelevantInformation(behaviorActivity);
             }
         }
     }
@@ -44,70 +48,106 @@ public class FlattenUMLActivityDiagram {
      * Recursively flatten the activity diagram.
      *
      * @param childBehavior The non-{@code null} activity diagram to be flattened.
-     * @param callBehaviorActionToReplace The call behavior action that calls the activity.
+     * @param callBehaviorActionToReplace The call behavior action that calls the activity. It can be null only when it
+     *     is called to flatten the outer most activity diagram.
      */
     public void flattenActivityDiagram(Activity childBehavior, CallBehaviorAction callBehaviorActionToReplace) {
-        // If 'childBehavior' is null then the behavior of the call behavior action is unspecified. Flattening cannot be done.
-        if (childBehavior == null) {
-            throw new RuntimeException("Expected a non-null activity diagram.");
-        }
-
-        for (ActivityNode node: new LinkedHashSet<>(childBehavior.getNodes())) {
-            if (node instanceof CallBehaviorAction) {
-                CallBehaviorAction actionNode = (CallBehaviorAction)node;
+        for (ActivityNode node: new ArrayList<>(childBehavior.getNodes())) {
+            if (node instanceof CallBehaviorAction actionNode) {
                 Behavior childDiagram = actionNode.getBehavior();
-                // Recursion to reach the leaf of the tree.
+
+                // Depth-first recursion if the activity diagram of the call behavior action is specified. Transform
+                // children first, for a bottom-up flattening. Otherwise, fattening cannot be done.
+                Verify.verify(childDiagram != null, String
+                        .format("The behavior of the call behavior action %s is unspecified.", actionNode.getName()));
                 flattenActivityDiagram((Activity)childDiagram, actionNode);
             }
         }
 
-        // Relocating edges when 'callBehaviorActionToReplace' is not null.
+        // Replace the call behavior action with the content of this activity diagram, and connect it with proper edges.
         if (callBehaviorActionToReplace != null) {
-            Activity tmp = EcoreUtil.copy(childBehavior);
+            Activity childBehaviorCopy = EcoreUtil.copy(childBehavior);
 
-            for (ActivityNode node: new LinkedHashSet<>(tmp.getNodes())) {
+            for (ActivityNode node: new ArrayList<>(childBehaviorCopy.getNodes())) {
                 // Set the activity for the node.
                 node.setActivity(callBehaviorActionToReplace.getActivity());
 
                 // Set the activity for all the edges to the activity of the call behavior action to be replaced.
-                for (ActivityEdge edge: new LinkedHashSet<>(node.getOutgoings())) {
+                for (ActivityEdge edge: new ArrayList<>(node.getOutgoings())) {
                     edge.setActivity(callBehaviorActionToReplace.getActivity());
                 }
-                for (ActivityEdge edge: new LinkedHashSet<>(node.getIncomings())) {
+                for (ActivityEdge edge: new ArrayList<>(node.getIncomings())) {
                     edge.setActivity(callBehaviorActionToReplace.getActivity());
                 }
-                // Relocate all outgoing edges out of the initial node, to all incoming neighbors of the call behavior
-                // action that is replaced.
-                if (node instanceof InitialNode) {
-                    for (ActivityEdge outgoingEdge: new LinkedHashSet<>(node.getOutgoings())) {
-                        for (ActivityEdge inComingEdge: new LinkedHashSet<>(
-                                callBehaviorActionToReplace.getIncomings()))
-                        {
-                            inComingEdge.setTarget(outgoingEdge.getTarget());
+
+                // Create a new edge for every pair of the outgoing edge from the behavior node and the incoming edge to
+                // the call behavior action. The target and source of the edge is set.
+                if (node instanceof InitialNode initialNode) {
+                    for (ActivityEdge outgoingEdge: new ArrayList<>(initialNode.getOutgoings())) {
+                        for (ActivityEdge incomingEdge: new ArrayList<>(callBehaviorActionToReplace.getIncomings())) {
+                            ControlFlow newEdge = FileHelper.FACTORY.createControlFlow();
+                            newEdge.setSource(incomingEdge.getSource());
+                            newEdge.setTarget(outgoingEdge.getTarget());
+                            // The guard of the new edge is set to the guard of the incoming edge of the call behavior
+                            // action. We ignore the guard of the outgoing edge of the initial node because the source
+                            // of this edge is not a decision node.
+                            newEdge.setGuard(incomingEdge.getGuard());
+                            newEdge.setActivity(callBehaviorActionToReplace.getActivity());
                         }
-                        // Destroy the outgoing edge from the initial node.
-                        outgoingEdge.destroy();
                     }
-                    // Destroy the initial node.
-                    node.destroy();
+                    // Destroy the initial node and the redundant edges.
+                    destoryRedundantNodesAndEdges(initialNode, callBehaviorActionToReplace);
                 }
-                // Relocate all incoming edges into the final node, to all outgoing neighbors of the call behavior action that is replaced.
-                if (node instanceof ActivityFinalNode) {
-                    for (ActivityEdge incomingEdge: new LinkedHashSet<>(node.getIncomings())) {
-                        for (ActivityEdge outgoingEdge: new LinkedHashSet<>(
-                                callBehaviorActionToReplace.getOutgoings()))
-                        {
-                            outgoingEdge.setSource(incomingEdge.getSource());
+
+                // Create a new edge for every pair of the incoming edge to the final node and the outgoing edge of
+                // the call behavior action. The target and source of the edge is set.
+                if (node instanceof ActivityFinalNode finalNode) {
+                    for (ActivityEdge incomingEdge: new ArrayList<>(finalNode.getIncomings())) {
+                        for (ActivityEdge outgoingEdge: new ArrayList<>(callBehaviorActionToReplace.getOutgoings())) {
+                            ControlFlow newEdge = FileHelper.FACTORY.createControlFlow();
+                            newEdge.setSource(incomingEdge.getSource());
+                            newEdge.setTarget(outgoingEdge.getTarget());
+                            // The guard of the new edge is set to the guard of the incoming edge of the final node. We
+                            // ignore the guard of the outgoing edge of the behavior action.
+                            newEdge.setGuard(incomingEdge.getGuard());
+                            newEdge.setActivity(callBehaviorActionToReplace.getActivity());
                         }
-                        // Destroy the incoming edge of the final node.
-                        incomingEdge.destroy();
                     }
-                    // Destroy the final node.
-                    node.destroy();
+                    // Destroy the final node and the redundant edges.
+                    destoryRedundantNodesAndEdges(finalNode, callBehaviorActionToReplace);
                 }
             }
+
             // Destroy the call behavior action being replaced.
             callBehaviorActionToReplace.destroy();
+        }
+    }
+
+    /**
+     * Destroy the initial node and its outgoing edges, the final node and its incoming edges, and the incoming and
+     * outgoing edges of the call behavior action.
+     *
+     * @param node The initial node or final node to destroy.
+     * @param action The call behavior action to replace
+     */
+    public static void destoryRedundantNodesAndEdges(ActivityNode node, CallBehaviorAction action) {
+        if (node instanceof InitialNode initialNode) {
+            for (ActivityEdge outgoingEdge: new ArrayList<>(initialNode.getOutgoings())) {
+                outgoingEdge.destroy();
+            }
+            for (ActivityEdge incomingEdge: new ArrayList<>(action.getIncomings())) {
+                incomingEdge.destroy();
+            }
+            initialNode.destroy();
+        }
+        if (node instanceof ActivityFinalNode finalNode) {
+            for (ActivityEdge incomingEdge: new ArrayList<>(finalNode.getIncomings())) {
+                incomingEdge.destroy();
+            }
+            for (ActivityEdge outgoingEdge: new ArrayList<>(action.getOutgoings())) {
+                outgoingEdge.destroy();
+            }
+            finalNode.destroy();
         }
     }
 }
--- plugins/com.github.tno.pokayoke.transform.uml/src/com/github/tno/pokayoke/transform/uml/ActivityHelper.java
+++ plugins/com.github.tno.pokayoke.transform.uml/src/com/github/tno/pokayoke/transform/uml/ActivityHelper.java
@@ -7,14 +7,11 @@ import java.util.stream.Collectors;
 
 import org.eclipse.uml2.uml.AcceptEventAction;
 import org.eclipse.uml2.uml.Activity;
-import org.eclipse.uml2.uml.ActivityEdge;
 import org.eclipse.uml2.uml.ControlFlow;
 import org.eclipse.uml2.uml.DecisionNode;
 import org.eclipse.uml2.uml.FinalNode;
 import org.eclipse.uml2.uml.InitialNode;
 import org.eclipse.uml2.uml.InputPin;
-import org.eclipse.uml2.uml.LiteralBoolean;
-import org.eclipse.uml2.uml.LiteralInteger;
 import org.eclipse.uml2.uml.LiteralString;
 import org.eclipse.uml2.uml.MergeNode;
 import org.eclipse.uml2.uml.ObjectFlow;
@@ -401,28 +398,5 @@ public class ActivityHelper {
         decisionToInnerMergeFlow.setGuard(decisionToInnerMergeGuard);
 
         return activity;
-    }
-
-    /**
-     * Removes irrelevant and redundant information from the given activity, like edge weights or redundant edge guards.
-     *
-     * @param activity The activity to clean up.
-     */
-    public static void removeIrrelevantInformation(Activity activity) {
-        // Remove any default weights from all edges.
-        for (ActivityEdge edge: activity.getEdges()) {
-            if (edge.getWeight() instanceof LiteralInteger literal && literal.getValue() == 0) {
-                edge.setWeight(null);
-            }
-        }
-
-        // Remove any default guards from all edges not coming out of decision nodes.
-        for (ActivityEdge edge: activity.getEdges()) {
-            if (!(edge.getSource() instanceof DecisionNode) && edge.getGuard() instanceof LiteralBoolean literal
-                    && literal.isValue())
-            {
-                edge.setGuard(null);
-            }
-        }
     }
 }
