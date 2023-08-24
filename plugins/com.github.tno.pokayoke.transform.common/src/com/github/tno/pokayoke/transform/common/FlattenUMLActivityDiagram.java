6954ed237c30c7f628a9ac53417197f7079bf327
Nan Yang <nanyang1993wendy@gmail.com>
2023-08-24 22:16:03 +0200

    Address comments.

--- plugins/com.github.tno.pokayoke.transform.common/src/com/github/tno/pokayoke/transform/common/FlattenUMLActivityDiagram.java
+++ plugins/com.github.tno.pokayoke.transform.common/src/com/github/tno/pokayoke/transform/common/FlattenUMLActivityDiagram.java
@@ -37,9 +37,9 @@ public class FlattenUMLActivityDiagram {
         Class contextClass = (Class)model.getMember("Context");
         // Transform all activity behaviors of 'contextClass'.
         for (Behavior behavior: new ArrayList<>(contextClass.getOwnedBehaviors())) {
-            if (behavior instanceof Activity behaviorActivity) {
-                flattenActivityDiagram(behaviorActivity, null);
-                UMLActivityUtils.removeIrrelevantInformation(behaviorActivity);
+            if (behavior instanceof Activity activity) {
+                flattenActivityDiagram(activity, null);
+                UMLActivityUtils.removeIrrelevantInformation(activity);
             }
         }
     }
@@ -48,16 +48,14 @@ public class FlattenUMLActivityDiagram {
      * Recursively flatten the activity diagram.
      *
      * @param childBehavior The non-{@code null} activity diagram to be flattened.
-     * @param callBehaviorActionToReplace The call behavior action that calls the activity. It can be null only when it
-     *     is called to flatten the outer most activity diagram.
+     * @param callBehaviorActionToReplace The call behavior action that calls the activity. It can be {@code null} only
+     *     when it is called to flatten the outer most activity diagram.
      */
     public void flattenActivityDiagram(Activity childBehavior, CallBehaviorAction callBehaviorActionToReplace) {
+        // Depth-first recursion. Transform children first, for a bottom-up flattening.
         for (ActivityNode node: new ArrayList<>(childBehavior.getNodes())) {
             if (node instanceof CallBehaviorAction actionNode) {
                 Behavior childDiagram = actionNode.getBehavior();
-
-                // Depth-first recursion if the activity diagram of the call behavior action is specified. Transform
-                // children first, for a bottom-up flattening. Otherwise, fattening cannot be done.
                 Verify.verify(childDiagram != null, String
                         .format("The behavior of the call behavior action %s is unspecified.", actionNode.getName()));
                 flattenActivityDiagram((Activity)childDiagram, actionNode);
@@ -73,81 +71,69 @@ public class FlattenUMLActivityDiagram {
                 node.setActivity(callBehaviorActionToReplace.getActivity());
 
                 // Set the activity for all the edges to the activity of the call behavior action to be replaced.
-                for (ActivityEdge edge: new ArrayList<>(node.getOutgoings())) {
+                for (ActivityEdge edge: node.getOutgoings()) {
                     edge.setActivity(callBehaviorActionToReplace.getActivity());
                 }
-                for (ActivityEdge edge: new ArrayList<>(node.getIncomings())) {
+                for (ActivityEdge edge: node.getIncomings()) {
                     edge.setActivity(callBehaviorActionToReplace.getActivity());
                 }
 
-                // Create a new edge for every pair of the outgoing edge from the behavior node and the incoming edge to
-                // the call behavior action. The target and source of the edge is set.
+                // Create a new edge for every pair of an outgoing edge from the activity's initial node and an incoming
+                // edge to the call behavior action. The edges are properly connected and given the appropriate
+                // properties, like guards.
                 if (node instanceof InitialNode initialNode) {
-                    for (ActivityEdge outgoingEdge: new ArrayList<>(initialNode.getOutgoings())) {
-                        for (ActivityEdge incomingEdge: new ArrayList<>(callBehaviorActionToReplace.getIncomings())) {
+                    for (ActivityEdge outgoingEdge: initialNode.getOutgoings()) {
+                        for (ActivityEdge incomingEdge: callBehaviorActionToReplace.getIncomings()) {
                             ControlFlow newEdge = FileHelper.FACTORY.createControlFlow();
                             newEdge.setSource(incomingEdge.getSource());
                             newEdge.setTarget(outgoingEdge.getTarget());
                             // The guard of the new edge is set to the guard of the incoming edge of the call behavior
                             // action. We ignore the guard of the outgoing edge of the initial node because the source
                             // of this edge is not a decision node.
-                            newEdge.setGuard(incomingEdge.getGuard());
+                            newEdge.setGuard(EcoreUtil.copy(incomingEdge.getGuard()));
                             newEdge.setActivity(callBehaviorActionToReplace.getActivity());
                         }
                     }
+
                     // Destroy the initial node and the redundant edges.
-                    destoryRedundantNodesAndEdges(initialNode, callBehaviorActionToReplace);
+                    for (ActivityEdge outgoingEdge: new ArrayList<>(initialNode.getOutgoings())) {
+                        outgoingEdge.destroy();
+                    }
+                    for (ActivityEdge incomingEdge: new ArrayList<>(callBehaviorActionToReplace.getIncomings())) {
+                        incomingEdge.destroy();
+                    }
+                    initialNode.destroy();
                 }
 
-                // Create a new edge for every pair of the incoming edge to the final node and the outgoing edge of
-                // the call behavior action. The target and source of the edge is set.
+                // Create a new edge for every pair of an incoming edge to the activity's final node and an outgoing
+                // edge of the call behavior action. The edges are properly connected and given the appropriate
+                // properties, like guards.
                 if (node instanceof ActivityFinalNode finalNode) {
-                    for (ActivityEdge incomingEdge: new ArrayList<>(finalNode.getIncomings())) {
-                        for (ActivityEdge outgoingEdge: new ArrayList<>(callBehaviorActionToReplace.getOutgoings())) {
+                    for (ActivityEdge incomingEdge: finalNode.getIncomings()) {
+                        for (ActivityEdge outgoingEdge: callBehaviorActionToReplace.getOutgoings()) {
                             ControlFlow newEdge = FileHelper.FACTORY.createControlFlow();
                             newEdge.setSource(incomingEdge.getSource());
                             newEdge.setTarget(outgoingEdge.getTarget());
                             // The guard of the new edge is set to the guard of the incoming edge of the final node. We
                             // ignore the guard of the outgoing edge of the behavior action.
-                            newEdge.setGuard(incomingEdge.getGuard());
+                            newEdge.setGuard(EcoreUtil.copy(incomingEdge.getGuard()));
                             newEdge.setActivity(callBehaviorActionToReplace.getActivity());
                         }
                     }
+
                     // Destroy the final node and the redundant edges.
-                    destoryRedundantNodesAndEdges(finalNode, callBehaviorActionToReplace);
+                    for (ActivityEdge incomingEdge: new ArrayList<>(finalNode.getIncomings())) {
+                        incomingEdge.destroy();
+                    }
+                    for (ActivityEdge outgoingEdge: new ArrayList<>(callBehaviorActionToReplace.getOutgoings())) {
+                        outgoingEdge.destroy();
+                    }
+                    finalNode.destroy();
                 }
             }
 
             // Destroy the call behavior action being replaced.
             callBehaviorActionToReplace.destroy();
-        }
-    }
-
-    /**
-     * Destroy the initial node and its outgoing edges, the final node and its incoming edges, and the incoming and
-     * outgoing edges of the call behavior action.
-     *
-     * @param node The initial node or final node to destroy.
-     * @param action The call behavior action to replace
-     */
-    public static void destoryRedundantNodesAndEdges(ActivityNode node, CallBehaviorAction action) {
-        if (node instanceof InitialNode initialNode) {
-            for (ActivityEdge outgoingEdge: new ArrayList<>(initialNode.getOutgoings())) {
-                outgoingEdge.destroy();
-            }
-            for (ActivityEdge incomingEdge: new ArrayList<>(action.getIncomings())) {
-                incomingEdge.destroy();
-            }
-            initialNode.destroy();
-        }
-        if (node instanceof ActivityFinalNode finalNode) {
-            for (ActivityEdge incomingEdge: new ArrayList<>(finalNode.getIncomings())) {
-                incomingEdge.destroy();
-            }
-            for (ActivityEdge outgoingEdge: new ArrayList<>(action.getOutgoings())) {
-                outgoingEdge.destroy();
-            }
-            finalNode.destroy();
         }
     }
 }
