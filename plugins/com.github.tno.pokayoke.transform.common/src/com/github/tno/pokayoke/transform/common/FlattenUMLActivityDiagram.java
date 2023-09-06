0b730acf53052ca68115decfce549b73f71957b1
Nan Yang <nanyang1993wendy@gmail.com>
2023-09-06 10:11:33 +0200

    Minor change

--- plugins/com.github.tno.pokayoke.transform.common/src/com/github/tno/pokayoke/transform/common/FlattenUMLActivityDiagram.java
+++ plugins/com.github.tno.pokayoke.transform.common/src/com/github/tno/pokayoke/transform/common/FlattenUMLActivityDiagram.java
@@ -92,7 +92,6 @@ public class FlattenUMLActivityDiagram {
                             // of this edge is not a decision node.
                             newEdge.setGuard(EcoreUtil.copy(incomingEdge.getGuard()));
                             newEdge.setActivity(callBehaviorActionToReplace.getActivity());
-
                         }
                     }
 
