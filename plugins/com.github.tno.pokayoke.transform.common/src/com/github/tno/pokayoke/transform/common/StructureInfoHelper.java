b0c46f3103ec720a85dd8203abe25f1267c62bf8
Nan Yang <nanyang1993wendy@gmail.com>
2023-10-03 16:37:48 +0200

    #38 Remove empty constructor.

--- plugins/com.github.tno.pokayoke.transform.common/src/com/github/tno/pokayoke/transform/common/StructureInfoHelper.java
+++ plugins/com.github.tno.pokayoke.transform.common/src/com/github/tno/pokayoke/transform/common/StructureInfoHelper.java
@@ -16,9 +16,6 @@ public class StructureInfoHelper {
 
     private int counter = 0;
 
-    public StructureInfoHelper() {
-    }
-
     public void incrementCounter() {
         counter = counter + 1;
     }
