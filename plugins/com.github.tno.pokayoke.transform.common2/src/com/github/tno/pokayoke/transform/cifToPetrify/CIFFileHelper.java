a2f007bfba6872d4f8a11ac5f5c7c06b9b7db1f3
Nan Yang <nanyang1993wendy@gmail.com>
2023-10-05 10:18:44 +0200

    #41 For solving build path problems.

---
+++ plugins/com.github.tno.pokayoke.transform.common2/src/com/github/tno/pokayoke/transform/cifToPetrify/CIFFileHelper.java
@@ -0,0 +1,23 @@
+
+package com.github.tno.pokayoke.transform.cifToPetrify;
+
+import java.nio.file.Path;
+
+import org.eclipse.escet.cif.io.CifReader;
+import org.eclipse.escet.cif.metamodel.cif.Specification;
+import org.eclipse.uml2.uml.Model;
+
+public class CIFFileHelper {
+    /**
+     * Read CIF {@link Specification} from given {@link Path}.
+     *
+     * @param path {@link Path} of input file.
+     * @return {@link Specification} read from input file.
+     */
+    public static Specification loadCIFSpec(Path path) {
+        CifReader reader = new CifReader();
+        reader.suppressWarnings = true;
+        reader.init(path.toString(), path.toAbsolutePath().toString(), false);
+        return reader.read();
+    }
+}
