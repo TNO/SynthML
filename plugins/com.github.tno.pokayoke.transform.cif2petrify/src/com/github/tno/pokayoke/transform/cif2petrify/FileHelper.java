bcd908f84e2820c4bfc9c4ae9b61637d8c0ca837
Nan Yang <nanyang1993wendy@gmail.com>
2023-10-06 11:53:24 +0200

    #41 Add store Petrify model function.

--- plugins/com.github.tno.pokayoke.transform.cif2petrify/src/com/github/tno/pokayoke/transform/cif2petrify/FileHelper.java
+++ plugins/com.github.tno.pokayoke.transform.cif2petrify/src/com/github/tno/pokayoke/transform/cif2petrify/FileHelper.java
@@ -1,22 +1,32 @@
 
 package com.github.tno.pokayoke.transform.cif2petrify;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.eclipse.escet.cif.io.CifReader;
 import org.eclipse.escet.cif.metamodel.cif.Specification;
 
 public class FileHelper {
-    /**
-     * Read CIF {@link Specification} from given {@link Path}.
-     *
-     * @param path {@link Path} of input file.
-     * @return {@link Specification} read from input file.
-     */
-    public static Specification loadCIFSpec(Path path) {
+    private FileHelper() {
+    }
+
+    public static Specification loadCIFSpec(String sourcePath) {
+        Path path = Paths.get(sourcePath);
         CifReader reader = new CifReader();
         reader.suppressWarnings = true;
         reader.init(path.toString(), path.toAbsolutePath().toString(), false);
         return reader.read();
+    }
+
+    public static void storePetrifySpec(String output, String targetPath) throws IOException {
+        FileWriter file = new FileWriter(targetPath);
+        BufferedWriter buffer = new BufferedWriter(file);
+        buffer.write(output);
+        buffer.flush();
+        buffer.close();
     }
 }
