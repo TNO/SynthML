8c0a3fd485c226d4873c18fc2af0916235f271fe
Nan Yang <nanyang1993wendy@gmail.com>
2023-10-10 09:57:45 +0200

    #41 Refactor based on Wytse's suggestions.

--- plugins/com.github.tno.pokayoke.transform.cif2petrify/src/com/github/tno/pokayoke/transform/cif2petrify/Cif2Petrify.java
+++ plugins/com.github.tno.pokayoke.transform.cif2petrify/src/com/github/tno/pokayoke/transform/cif2petrify/Cif2Petrify.java
@@ -2,19 +2,17 @@
 package com.github.tno.pokayoke.transform.cif2petrify;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.escet.cif.metamodel.cif.Component;
+import org.eclipse.escet.cif.common.CifEventUtils;
 import org.eclipse.escet.cif.metamodel.cif.Specification;
-import org.eclipse.escet.cif.metamodel.cif.automata.Alphabet;
 import org.eclipse.escet.cif.metamodel.cif.automata.Automaton;
 import org.eclipse.escet.cif.metamodel.cif.automata.Edge;
-import org.eclipse.escet.cif.metamodel.cif.automata.EdgeEvent;
 import org.eclipse.escet.cif.metamodel.cif.automata.Location;
-import org.eclipse.escet.cif.metamodel.cif.expressions.EventExpression;
-import org.eclipse.escet.cif.metamodel.cif.expressions.Expression;
+import org.eclipse.escet.cif.metamodel.cif.declarations.Event;
+
+import com.google.common.base.Preconditions;
 
 /** Transforms CIF state spaces to Petrify input. */
 public class Cif2Petrify {
@@ -43,78 +41,64 @@ public class Cif2Petrify {
         stringBuilder.append("\n");
         stringBuilder.append(".dummy start end ");
 
-        EList<Component> components = specification.getComponents();
+        // Get the state space.
+        Optional<Automaton> possibleStatespace = specification.getComponents().stream()
+                .filter(c -> c instanceof Automaton a && a.getName().equals("statespace")).map(c -> (Automaton)c)
+                .findFirst();
 
-        // Iterate over components to extract information from the component named 'statespace'.
-        for (Component component: components) {
-            if (component.getName().equals("statespace")) {
-                Automaton automaton = (Automaton)component;
+        Preconditions.checkArgument(possibleStatespace.isPresent(),
+                "Expected the CIF specification to include a state space automaton.");
 
-                // Extract the name of events.
-                Alphabet alphabet = automaton.getAlphabet();
-                List<String> eventNames = new ArrayList<>();
-                for (Expression expression: alphabet.getEvents()) {
-                    EventExpression eventExpression = (EventExpression)expression;
-                    eventNames.add(eventExpression.getEvent().getName());
-                }
+        Automaton automaton = possibleStatespace.get();
 
-                // Append the declaration of events.
-                stringBuilder.append(String.join(" ", eventNames));
+        // Extract the name of events.
+        List<String> eventNames = CifEventUtils.getAlphabet(automaton).stream().map(Event::getName).toList();
+
+        // Append the declaration of events.
+        stringBuilder.append(String.join(" ", eventNames));
+        stringBuilder.append("\n");
+
+        stringBuilder.append(".state graph");
+        stringBuilder.append("\n");
+
+        // Iterate over all locations in the state space and translate all edges.
+        for (Location location: automaton.getLocations()) {
+            // Translate initial locations.
+            if (!location.getInitials().isEmpty()) {
+                // Append the starting edge.
+                stringBuilder.append(String.format("loc0 start %s", location.getName()));
                 stringBuilder.append("\n");
+            }
 
-                stringBuilder.append(".state graph");
+            // Translate marked locations.
+            if (!location.getMarkeds().isEmpty()) {
+                Preconditions.checkArgument(location.getEdges().isEmpty(),
+                        "Expected a marked location does not have outgoing edges");
+
+                // Append the ending edge.
+                stringBuilder.append(String.format("%s end loc0", location.getName()));
                 stringBuilder.append("\n");
+            }
 
-                List<Location> markedLocactions = new ArrayList<>();
-
-                // Iterate over locations to extract edge info.
-                for (Location location: automaton.getLocations()) {
-                    // Identify the initial location and append the first edge.
-                    List<Expression> initialExpressions = location.getInitials();
-                    if (initialExpressions.size() > 0) {
-                        stringBuilder.append(String.format("loc0 start %s", location.getName()));
-                        stringBuilder.append("\n");
-                    }
-
-                    // Identify the marked locations.
-                    if (location.getMarkeds().size() > 0 & location.getEdges().size() == 0) {
-                        markedLocactions.add(location);
-                    }
-
-                    // Iterate over edges to extract edge events and append them to the string.
-                    for (Edge edge: location.getEdges()) {
-                        List<String> edgeEvents = new ArrayList<>();
-
-                        // Collect the name of edge events.
-                        for (EdgeEvent edgeEvent: edge.getEvents()) {
-                            EventExpression expression = (EventExpression)edgeEvent.getEvent();
-                            String eventName = expression.getEvent().getName();
-                            edgeEvents.add(eventName);
-                        }
-
-                        // Append the edge.
-                        if (edgeEvents.size() > 0) {
-                            String edgeString = String.format("%s %s %s", location.getName(),
-                                    String.join(",", edgeEvents), edge.getTarget().getName());
-                            stringBuilder.append(edgeString);
-                            stringBuilder.append("\n");
-                        }
-                    }
-                }
-                // Append the looping edge.
-                for (Location markedLocation: markedLocactions) {
-                    stringBuilder.append(String.format("%s end loc0", markedLocation.getName()));
+            // Translate all edges that go out of the current location.
+            for (Edge edge: location.getEdges()) {
+                for (Event edgeEvent: CifEventUtils.getEvents(edge)) {
+                    // Append the edge.
+                    String edgeString = String.format("%s %s %s", location.getName(), edgeEvent.getName(),
+                            edge.getTarget().getName());
+                    stringBuilder.append(edgeString);
                     stringBuilder.append("\n");
                 }
-
-                // Append the marking string.
-                stringBuilder.append(".marking {loc0}");
-                stringBuilder.append("\n");
             }
         }
 
+        // Append the marking string.
+        stringBuilder.append(".marking {loc0}");
+        stringBuilder.append("\n");
+
         // Append the ending string.
         stringBuilder.append(".end");
+        stringBuilder.append("\n");
 
         return stringBuilder.toString();
     }
