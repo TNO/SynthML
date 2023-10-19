5c07d54f85486bcf61fb5b78a93741ba2abb4cdf
Nan Yang <nanyang1993wendy@gmail.com>
2023-10-19 21:29:18 +0200

    #89 Check location's name is not null.

--- plugins/com.github.tno.pokayoke.transform.cif2petrify/src/com/github/tno/pokayoke/transform/cif2petrify/Cif2Petrify.java
+++ plugins/com.github.tno.pokayoke.transform.cif2petrify/src/com/github/tno/pokayoke/transform/cif2petrify/Cif2Petrify.java
@@ -68,6 +68,7 @@ public class Cif2Petrify {
         for (Location location: automaton.getLocations()) {
             String locationName = location.getName();
 
+            Preconditions.checkArgument(locationName != null, "Expected locations to have a name.");
             Preconditions.checkArgument(!locationName.equals("loc0"),
                     "Expected no locations in the state space automaton to be named 'loc0'.");
             Preconditions.checkArgument(!location.getEdges().isEmpty() || !location.getMarkeds().isEmpty(),
@@ -91,8 +92,10 @@ public class Cif2Petrify {
             // Translate all edges that go out of the current location.
             for (Edge edge: location.getEdges()) {
                 for (Event edgeEvent: CifEventUtils.getEvents(edge)) {
-                    String targetLocationName = edge.getTarget() == null ? location.getName() : edge.getTarget().getName();
-                    String edgeString = String.format("%s %s %s", locationName, edgeEvent.getName(), targetLocationName);
+                    String targetLocationName = edge.getTarget() == null ? location.getName()
+                            : edge.getTarget().getName();
+                    String edgeString = String.format("%s %s %s", locationName, edgeEvent.getName(),
+                            targetLocationName);
                     stringBuilder.append(edgeString);
                     stringBuilder.append("\n");
                 }
