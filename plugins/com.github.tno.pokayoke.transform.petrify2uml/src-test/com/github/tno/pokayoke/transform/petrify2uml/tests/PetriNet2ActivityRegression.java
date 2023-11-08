3d620a58e7019a33b1f669d2fee771d1ffc50c75
Nan Yang <nanyang1993wendy@gmail.com>
2023-11-08 11:14:49 +0100

    #117 Add test factory for UML (not comparing IDs).

--- plugins/com.github.tno.pokayoke.transform.petrify2uml/src-test/com/github/tno/pokayoke/transform/petrify2uml/tests/PetriNet2ActivityRegression.java
+++ plugins/com.github.tno.pokayoke.transform.petrify2uml/src-test/com/github/tno/pokayoke/transform/petrify2uml/tests/PetriNet2ActivityRegression.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,7 +18,12 @@ import com.github.tno.pokayoke.transform.tests.common.Regression;
 class PetriNet2ActivityRegression extends Regression {
     public static final String INPUT_FILE_EXTENSION = "out";
 
-    public static final String OUTPUT_FILE_EXTENSION = "uml";
+    public static final String OUTPUT_FILE_EXTENSION = "umltst";
+
+    @BeforeAll
+    public static void setup() {
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(OUTPUT_FILE_EXTENSION, new UMLTestFactory());
+    }
 
     public static Stream<? extends Arguments> provideArguments() throws Exception {
         return Regression.provideArguments(INPUT_FILE_EXTENSION, OUTPUT_FILE_EXTENSION);
