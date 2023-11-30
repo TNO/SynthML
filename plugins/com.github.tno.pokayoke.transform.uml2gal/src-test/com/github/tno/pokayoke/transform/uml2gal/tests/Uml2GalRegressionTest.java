0bc45b9c5759bf0da927ed9b2a716b39e915bafe
wytseoortwijn <wytseoortwijn@gmail.com>
2023-11-30 14:39:09 +0100

    #7 Added a simple regression test

---
+++ plugins/com.github.tno.pokayoke.transform.uml2gal/src-test/com/github/tno/pokayoke/transform/uml2gal/tests/Uml2GalRegressionTest.java
@@ -0,0 +1,36 @@
+
+package com.github.tno.pokayoke.transform.uml2gal.tests;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.github.tno.pokayoke.transform.tests.common.RegressionTest;
+import com.github.tno.pokayoke.transform.uml2gal.Uml2GalTranslationHelper;
+
+/** Regression tests for translating UML models to GAL specifications. */
+public class Uml2GalRegressionTest extends RegressionTest {
+    public static final String INPUT_FILE_EXTENSION = "uml";
+
+    public static final String OUTPUT_FILE_EXTENSION = "gal";
+
+    public static Stream<? extends Arguments> provideArguments() throws Exception {
+        return RegressionTest.provideArguments(INPUT_FILE_EXTENSION, OUTPUT_FILE_EXTENSION);
+    }
+
+    @Override
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    public void regressionTest(Path inputPath, Path expectedPath, Path outputPath, String message) throws IOException {
+        super.regressionTest(inputPath, expectedPath, outputPath, message);
+    }
+
+    @Override
+    protected void actTest(Path inputPath, Path outputPath) throws IOException {
+        Uml2GalTranslationHelper.translateCifAnnotatedModel(inputPath.toString(), outputPath.toString());
+    }
+}
--- plugins/com.github.tno.pokayoke.transform.uml2gal/src/com/github/tno/pokayoke/transform/uml2gal/Uml2GalTranslationHelper.java
+++ plugins/com.github.tno.pokayoke.transform.uml2gal/src/com/github/tno/pokayoke/transform/uml2gal/Uml2GalTranslationHelper.java
@@ -1,18 +1,60 @@
 
 package com.github.tno.pokayoke.transform.uml2gal;
 
+import java.io.IOException;
 import java.util.Collection;
 
+import org.eclipse.uml2.uml.Model;
+
+import com.github.tno.pokayoke.transform.common.FileHelper;
 import com.google.common.base.Preconditions;
 
 import fr.lip6.move.gal.And;
 import fr.lip6.move.gal.BooleanExpression;
 import fr.lip6.move.gal.GalFactory;
+import fr.lip6.move.gal.Specification;
+import fr.lip6.move.serialization.SerializationUtil;
 
+/** Helper for translating UML models to GAL specifications. */
 public class Uml2GalTranslationHelper {
     static final GalFactory FACTORY = GalFactory.eINSTANCE;
 
     private Uml2GalTranslationHelper() {
+    }
+
+    /**
+     * Loads a GAL specification from the specified path.
+     *
+     * @param pathName The path from which to read the GAL specification.
+     * @return The loaded GAL specification.
+     */
+    public static Specification load(String pathName) {
+        SerializationUtil.setStandalone(true);
+        return SerializationUtil.fileToGalSystem(pathName);
+    }
+
+    /**
+     * Stores the given GAL specification to the specified path.
+     *
+     * @param specification The GAL specification to store.
+     * @param pathName The path to store the specification.
+     * @throws IOException Thrown in case the specification could not be stored.
+     */
+    public static void store(Specification specification, String pathName) throws IOException {
+        SerializationUtil.systemToFile(specification, pathName, true);
+    }
+
+    /**
+     * Translates a CIF-annotated UML model to a GAL specification.
+     *
+     * @param sourcePath The path to load the UML model.
+     * @param targetPath The path to store the translated GAL specification.
+     * @throws IOException Thrown in case the model could not be loaded or the specification be stored.
+     */
+    public static void translateCifAnnotatedModel(String sourcePath, String targetPath) throws IOException {
+        Model model = FileHelper.loadModel(sourcePath);
+        Specification specification = new CifAnnotatedUml2GalTranslator(sourcePath).translate(model);
+        store(specification, targetPath);
     }
 
     static And combineAsAnd(BooleanExpression left, BooleanExpression right) {
