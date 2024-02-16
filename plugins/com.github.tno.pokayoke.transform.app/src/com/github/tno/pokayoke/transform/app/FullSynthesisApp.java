19264fed64d9f0694e531f2e36fae40b474aea71
Nan Yang <nanyang1993wendy@gmail.com>
2024-02-16 14:26:06 +0100

    #169 Improve Javadoc and refactor.

--- plugins/com.github.tno.pokayoke.transform.app/src/com/github/tno/pokayoke/transform/app/FullSynthesisApp.java
+++ plugins/com.github.tno.pokayoke.transform.app/src/com/github/tno/pokayoke/transform/app/FullSynthesisApp.java
@@ -6,12 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import org.apache.commons.io.FilenameUtils;
 import org.eclipse.emf.ecore.util.EcoreUtil;
@@ -31,6 +30,7 @@ import org.eclipse.escet.cif.metamodel.cif.automata.Location;
 import org.eclipse.escet.cif.metamodel.cif.declarations.Event;
 import org.eclipse.escet.cif.metamodel.cif.expressions.StringExpression;
 import org.eclipse.escet.common.app.framework.AppEnv;
+import org.eclipse.escet.common.java.Sets;
 
 import com.github.tno.pokayoke.transform.cif2petrify.Cif2Petrify;
 import com.github.tno.pokayoke.transform.cif2petrify.FileHelper;
@@ -115,16 +115,18 @@ public class FullSynthesisApp {
     }
 
     /**
-     * Remove state annotations from intermediate states, states that have uncontrollable events on its outgoing edges.
+     * Remove state annotations from intermediate states, states that have uncontrollable events on their outgoing
+     * edges.
      *
-     * @param spec CIF specification in which state annotations to be removed.
+     * @param spec CIF specification from which to remove state annotations.
      * @param outputFilePath The output path of the specification.
      * @param outputFolderPath The path of the output folder.
      */
     private static void reduceStateAnnotations(Specification spec, Path outputFilePath, Path outputFolderPath) {
-        Set<Event> events = CifCollectUtils.collectEvents(spec, new ArrayList<>()).stream().collect(Collectors.toSet());
+        Set<Event> events = CifCollectUtils.collectEvents(spec, new LinkedHashSet<>());
         Set<Event> uncontrollableEvents = events.stream().filter(event -> !event.getControllable())
-                .collect(Collectors.toSet());
+                .collect(Sets.toSet());
+        Set<Event> controllableEvents = events.stream().filter(event -> event.getControllable()).collect(Sets.toSet());
 
         // Obtain the automaton in the CIF specification.
         List<Automaton> automata = CifCollectUtils.collectAutomata(spec, new ArrayList<>());
@@ -134,12 +136,17 @@ public class FullSynthesisApp {
 
         for (Location loc: automaton.getLocations()) {
             Set<Event> edgeEvents = loc.getEdges().stream().flatMap(edge -> CifEventUtils.getEvents(edge).stream())
-                    .collect(Collectors.toSet());
+                    .collect(Sets.toSet());
 
-            if (uncontrollableEvents.containsAll(edgeEvents) && !edgeEvents.isEmpty()) {
-                List<Annotation> annotationToRemove = loc.getAnnotations().stream()
-                        .filter(annotation -> annotation.getName().equals("state")).toList();
-                loc.getAnnotations().removeAll(annotationToRemove);
+            if (!edgeEvents.isEmpty()) {
+                if (uncontrollableEvents.containsAll(edgeEvents)) {
+                    List<Annotation> annotationToRemove = loc.getAnnotations().stream()
+                            .filter(annotation -> annotation.getName().equals("state")).toList();
+                    loc.getAnnotations().removeAll(annotationToRemove);
+                } else {
+                    Verify.verify(controllableEvents.containsAll(edgeEvents),
+                            "Expected that the events of an edge are either controllable events or uncontrollable events.");
+                }
             }
         }
         try {
@@ -153,7 +160,7 @@ public class FullSynthesisApp {
     /**
      * Remove state annotations.
      *
-     * @param spec CIF specification in which state annotations to be removed.
+     * @param spec CIF specification from which to remove state annotations.
      * @param outputFilePath The output path of the specification.
      * @param outputFolderPath The path of the output folder.
      */
@@ -208,8 +215,11 @@ public class FullSynthesisApp {
                         "Expected the annotation to have exactly one argument.");
                 AnnotationArgument argument = annotation.getArguments().get(0);
                 String mappedLocationName = ((StringExpression)argument.getValue()).getValue();
-                Location mappedLocation = map2.keySet().stream().filter(loc -> loc.getName().equals(mappedLocationName))
-                        .toList().get(0);
+                List<Location> mappedLocations = map2.keySet().stream()
+                        .filter(loc -> loc.getName().equals(mappedLocationName)).toList();
+                Preconditions.checkArgument(mappedLocations.size() == 1,
+                        String.format("Expected that there is only one location named %s.", mappedLocationName));
+                Location mappedLocation = mappedLocations.get(0);
                 mappedAnnotations.addAll(map2.get(mappedLocation));
             }
 
