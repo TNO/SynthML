201f0d1e8ce8e2549c87690be44bc422020db194
Yuri Blankenstein <yuri.blankenstein@tno.nl>
2024-02-21 15:53:43 +0100

    #172 extracted uml profile utility for reading/writing attributes

---
+++ plugins/com.github.tno.pokayoke.uml.profile.util/src/com/github/tno/pokayoke/uml/profile/util/GuardsEffectsUtil.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package com.github.tno.pokayoke.uml.profile.util;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.eclipse.escet.cif.parser.CifExpressionParser;
+import org.eclipse.escet.cif.parser.CifUpdatesParser;
+import org.eclipse.escet.cif.parser.ast.automata.AUpdate;
+import org.eclipse.escet.cif.parser.ast.expressions.AExpression;
+import org.eclipse.escet.setext.runtime.exceptions.ParseException;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.OpaqueAction;
+import org.eclipse.uml2.uml.Stereotype;
+import org.obeonetwork.dsl.uml2.core.api.services.ReusedDescriptionServices;
+
+import PokaYoke.PokaYokePackage;
+
+/**
+ * 
+ */
+public class GuardsEffectsUtil {
+	private static final String QN_GUARD_EFFECTS_ACTION = "PokaYoke::GuardEffectsAction";
+	private static final String QN_GUARD_EFFECTS_ACTION__GUARD = PokaYokePackage.Literals.GUARD_EFFECTS_ACTION__GUARD.getName();
+	private static final String QN_GUARD_EFFECTS_ACTION__EFFECTS = PokaYokePackage.Literals.GUARD_EFFECTS_ACTION__EFFECTS.getName();
+
+	public static String getGuard(OpaqueAction action) {
+		return getAppliedStereotype(action, QN_GUARD_EFFECTS_ACTION)
+				.map(st -> (String) action.getValue(st, QN_GUARD_EFFECTS_ACTION__GUARD)).orElse(null);
+	}
+
+	public static void setGuard(OpaqueAction action, String newValue) {
+		Stereotype st = applyStereotype(action, QN_GUARD_EFFECTS_ACTION);
+		action.setValue(st, QN_GUARD_EFFECTS_ACTION__GUARD, newValue);
+	}
+
+	public static AExpression getGuardExpression(OpaqueAction action) throws ParseException {
+		Optional<String> guard = getAppliedStereotype(action, QN_GUARD_EFFECTS_ACTION)
+				.map(st -> (String) action.getValue(st, QN_GUARD_EFFECTS_ACTION__GUARD));
+		if (guard.isEmpty() || guard.get().isEmpty()) {
+			// Not stereotyped or guard not set, return null
+			return null;
+		}
+		CifExpressionParser expressionParser = new CifExpressionParser();
+		return expressionParser.parseString(guard.get(), action.eResource().getURI().toString());
+	}
+	
+	public static String getEffects(OpaqueAction action) {
+		return getAppliedStereotype(action, QN_GUARD_EFFECTS_ACTION)
+				.map(st -> (String) action.getValue(st, QN_GUARD_EFFECTS_ACTION__EFFECTS)).orElse(null);
+	}
+
+	public static void setEffects(OpaqueAction action, String newValue) {
+		Stereotype st = applyStereotype(action, QN_GUARD_EFFECTS_ACTION);
+		action.setValue(st, QN_GUARD_EFFECTS_ACTION__EFFECTS, newValue);
+	}
+	
+	public static List<AUpdate> getEffectsUpdates(OpaqueAction action) throws ParseException {
+		Optional<String> effects = getAppliedStereotype(action, QN_GUARD_EFFECTS_ACTION)
+				.map(st -> (String) action.getValue(st, QN_GUARD_EFFECTS_ACTION__EFFECTS));
+		if (effects.isEmpty() || effects.get().isEmpty()) {
+			// Not stereotyped or effects not set, skip validation
+			return null;
+		}
+		CifUpdatesParser updatesParser = new CifUpdatesParser();
+		return updatesParser.parseString(effects.get(), action.eResource().getURI().toString());
+	}
+
+	private static Optional<Stereotype> getAppliedStereotype(Element element, String qualifiedName) {
+		Stereotype stereotype =  element.getAppliedStereotype(qualifiedName);
+		return stereotype == null ? Optional.empty() : Optional.of(stereotype);
+	}
+
+	private static Stereotype applyStereotype(Element element, String qualifiedName) {
+		Optional<Stereotype> st = getAppliedStereotype(element, qualifiedName);
+		if (st.isPresent()) {
+			return st.get();
+		}
+
+		// FIXME: Refactor this to avoid dependency on UML designer plugin
+		ReusedDescriptionServices rds = new ReusedDescriptionServices();
+		st = rds.getAllStereotypesAndProfiles(element).stream()
+				.filter(sp -> sp instanceof Stereotype
+						&& Objects.equals(((Stereotype) sp).getQualifiedName(), qualifiedName))
+				.map(Stereotype.class::cast)
+				.findAny();
+		if (st.isEmpty()) {
+			throw new IllegalArgumentException("Stereotype not found: " + qualifiedName);
+		}
+		
+		Stereotype stereotype = st.get();
+		org.eclipse.uml2.uml.Package elementPackage = element.getNearestPackage();
+		if (!elementPackage.isProfileApplied(stereotype.getProfile())) {
+			elementPackage.applyProfile(stereotype.getProfile());
+		}
+		element.applyStereotype(stereotype);
+		return stereotype;
+	}
+}
