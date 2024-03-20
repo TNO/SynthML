595e99de9fdb380869578beef30637a9a7443ebc
Nan Yang <nanyang1993wendy@gmail.com>
2024-03-20 09:52:32 +0100

    #181 Test (local backup)

---
+++ plugins/com.github.tno.pokayoke.transform.app/src-test/com/github/tno/pokayoke/transform/app/tests/FullSynthesisAppRegressionTest.java
@@ -0,0 +1,38 @@
+
+package com.github.tno.pokayoke.transform.app.tests;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.github.tno.pokayoke.transform.app.FullSynthesisApp;
+import com.github.tno.pokayoke.transform.tests.common.RegressionTest;
+
+/**
+ * Regression tests.
+ */
+class FullSynthesisAppRegressionTest extends RegressionTest {
+    public static final String INPUT_FILE_EXTENSION = "cif";
+
+    public static final String OUTPUT_FILE_EXTENSION = "uml";
+
+    public static Stream<? extends Arguments> provideArguments() throws Exception {
+        return RegressionTest.provideArguments(INPUT_FILE_EXTENSION, OUTPUT_FILE_EXTENSION);
+    }
+
+    @Override
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    public void regressionTest(Path inputPath, Path expectedPath, Path outputPath, String message) throws IOException {
+        super.regressionTest(inputPath, expectedPath, outputPath, message);
+    }
+
+    @Override
+    protected void actTest(Path inputPath, Path outputPath) throws IOException {
+        FullSynthesisApp.performFullSynthesis(inputPath, outputPath);
+    }
+}
