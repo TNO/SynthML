6da6d108b6a23a3386aff8f18756a5baeabb8f66
Yuri Blankenstein <yuri.blankenstein@tno.nl>
2024-04-29 09:37:54 +0200

    #237 Refactored Poka Yoke type checking

--- plugins/com.github.tno.pokayoke.uml.profile.util/src/com/github/tno/pokayoke/uml/profile/util/PokaYokeUmlProfileUtil.java
+++ plugins/com.github.tno.pokayoke.uml.profile.util/src/com/github/tno/pokayoke/uml/profile/util/PokaYokeUmlProfileUtil.java
@@ -5,13 +5,18 @@ import java.util.Optional;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.uml2.uml.Action;
+import org.eclipse.uml2.uml.ControlFlow;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.uml2.uml.OpaqueExpression;
 import org.eclipse.uml2.uml.Package;
 import org.eclipse.uml2.uml.Profile;
 import org.eclipse.uml2.uml.Property;
 import org.eclipse.uml2.uml.Stereotype;
+import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UMLPlugin;
+
+import com.google.common.base.Strings;
 
 import PokaYoke.GuardEffectsAction;
 import PokaYoke.PokaYokePackage;
@@ -66,6 +71,44 @@ public class PokaYokeUmlProfileUtil {
         action.setValue(st, PROP_GUARD_EFFECTS_ACTION_EFFECTS, newValue);
     }
 
+    /**
+     * Applies the Poka Yoke UML Profile and sets the
+     * {@link ControlFlow#setGuard(org.eclipse.uml2.uml.ValueSpecification) guard} for {@code controlFlow}.
+     *
+     * @param controlFlow The control flow to set the guard value on.
+     * @param newValue The new value of the guard.
+     */
+    public static void setGuard(ControlFlow controlFlow, String newValue) {
+        applyPokaYokeProfile(controlFlow);
+        if (Strings.isNullOrEmpty(newValue)) {
+            controlFlow.setGuard(UMLFactory.eINSTANCE.createLiteralNull());
+            return;
+        }
+        OpaqueExpression expression = UMLFactory.eINSTANCE.createOpaqueExpression();
+        expression.getLanguages().add("CIF");
+        expression.getBodies().add(newValue);
+        controlFlow.setGuard(expression);
+    }
+
+    /**
+     * Applies the Poka Yoke UML Profile and sets the {@link Property#setDefault(String) default value} for
+     * {@code property}.
+     *
+     * @param property The property to set the default value on.
+     * @param newValue The new default value of the property.
+     */
+    public static void setDefaultValue(Property property, String newValue) {
+        applyPokaYokeProfile(property);
+        if (Strings.isNullOrEmpty(newValue)) {
+            property.setDefaultValue(UMLFactory.eINSTANCE.createLiteralNull());
+            return;
+        }
+        OpaqueExpression expression = UMLFactory.eINSTANCE.createOpaqueExpression();
+        expression.getLanguages().add("CIF");
+        expression.getBodies().add(newValue);
+        property.setDefaultValue(expression);
+    }
+
     public static Optional<Profile> getAppliedProfile(Element element, String qualifiedName) {
         Package pkg = element.getNearestPackage();
         return Optional.ofNullable(pkg.getAppliedProfile(qualifiedName));
@@ -77,11 +120,6 @@ public class PokaYokeUmlProfileUtil {
 
     public static void unapplyStereotype(Element element, String qualifiedName) {
         getAppliedStereotype(element, qualifiedName).ifPresent(st -> element.unapplyStereotype(st));
-    }
-
-    public static void setPropertyDefaultValue(Property property, String newValue) {
-        applyPokaYokeProfile(property);
-        property.setDefault(newValue);
     }
 
     private static Profile getPokaYokeProfile(Element context) {
