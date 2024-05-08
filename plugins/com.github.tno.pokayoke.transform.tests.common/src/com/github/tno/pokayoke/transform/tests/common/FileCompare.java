4b5db93f05cd751a52a14b846c42c0735b3eae98
Nan Yang <nanyang1993wendy@gmail.com>
2024-05-08 09:42:39 +0200

    #139 Revert change to to get expectedPath back.

--- plugins/com.github.tno.pokayoke.transform.tests.common/src/com/github/tno/pokayoke/transform/tests/common/FileCompare.java
+++ plugins/com.github.tno.pokayoke.transform.tests.common/src/com/github/tno/pokayoke/transform/tests/common/FileCompare.java
@@ -35,37 +35,36 @@ public class FileCompare {
         List<Path> expectedPaths = Files.walk(expectedPath).filter(Files::isRegularFile).toList();
         List<Path> actualPaths = Files.walk(actualPath).filter(Files::isRegularFile).toList();
 
-        checkFileListsEqual(expectedPaths, actualPaths, message);
+        checkFileListsEqual(expectedPath, expectedPaths, actualPath, actualPaths, message);
     }
 
     /**
      * Check whether two given lists of files are equal content-wise. Files are matched based on paths.
      *
-     * @param expectedPaths List of paths of expected output files.
-     * @param actualPaths List of paths of actual output files.
+     * @param expectedFolderPath Path to root directory containing expected files.
+     * @param expectedItemPaths List of paths of expected output files.
+     * @param actualFolderPath Path to root directory containing actual output files.
+     * @param actualItemPaths List of paths of actual output files.
      * @param message The message to be used when the assertion fails.
      * @throws IOException when loading one of the files to compare fails.
      */
-    private static void checkFileListsEqual(List<Path> expectedPaths, List<Path> actualPaths, String message)
-            throws IOException
+    private static void checkFileListsEqual(Path expectedFolderPath, List<Path> expectedItemPaths,
+            Path actualFolderPath, List<Path> actualItemPaths, String message) throws IOException
     {
-        List<String> expectedFileNames = expectedPaths.stream().map(p -> p.getFileName().toString())
+        List<String> expectedItemStrings = expectedItemPaths.stream()
+                .map(p -> expectedFolderPath.relativize(p).toString()).collect(Collectors.toList());
+        List<String> actualItemStrings = actualItemPaths.stream().map(p -> actualFolderPath.relativize(p).toString())
                 .collect(Collectors.toList());
 
-        List<String> actualFileNames = actualPaths.stream().map(p -> p.getFileName().toString())
-                .collect(Collectors.toList());
+        Collections.sort(expectedItemStrings);
+        Collections.sort(actualItemStrings);
 
-        Collections.sort(expectedFileNames);
-        Collections.sort(actualFileNames);
+        assertLinesMatch(expectedItemStrings, actualItemStrings);
 
-        // Compare if the expected and actual file names are the same.
-        assertLinesMatch(expectedFileNames, actualFileNames);
-
-        // Compare if the content of the files are the same.
-        for (Path actualItemPath: actualPaths) {
-            String actualFileName = actualItemPath.getFileName().toString();
-            Path expectedFilePath = actualItemPath.getParent().resolve(actualFileName);
-            PathAssertions.assertContentsMatch(expectedFilePath, actualItemPath, message);
+        for (String actualItemString: actualItemStrings) {
+            Path expectedFilePath = expectedFolderPath.resolve(actualItemString);
+            Path actualFilePath = actualFolderPath.resolve(actualItemString);
+            PathAssertions.assertContentsMatch(expectedFilePath, actualFilePath, message);
         }
     }
 }
