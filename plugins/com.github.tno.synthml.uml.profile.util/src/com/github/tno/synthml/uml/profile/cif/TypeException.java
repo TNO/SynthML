840269903fe61c5545ad90c4ae19d31d129d59be
AndreaPuffo <andrea.peruffo.05@gmail.com>
2025-05-19 21:28:53 +0200

    #361 Add synthml.uml.profile.util

---
+++ plugins/com.github.tno.synthml.uml.profile.util/src/com/github/tno/synthml/uml/profile/cif/TypeException.java
@@ -0,0 +1,51 @@
+
+package com.github.tno.synthml.uml.profile.cif;
+
+import org.eclipse.escet.common.java.TextPosition;
+
+public class TypeException extends RuntimeException {
+    private static final long serialVersionUID = 6860730254242254224L;
+
+    private final TextPosition position;
+
+    /**
+     * Constructs a new runtime exception with the specified detail {@code message}.
+     *
+     * @param message The detail message. The detail message is saved for later retrieval by the {@link #getMessage()}
+     *     method.
+     */
+    public TypeException(String message) {
+        this(message, null);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified detail {@code message} and {@code position}.
+     *
+     * @param message The detail message. The detail message is saved for later retrieval by the {@link #getMessage()}
+     *     method.
+     * @param position The text position of the problem. The position is saved for later retrieval by the
+     *     {@link #getMessage()} method.
+     */
+    public TypeException(String message, TextPosition position) {
+        super(message);
+        this.position = position;
+    }
+
+    /**
+     * Returns the position.
+     *
+     * @return The position
+     */
+    public TextPosition getPosition() {
+        return position;
+    }
+
+    @Override
+    public String getMessage() {
+        if (position == null) {
+            return super.getMessage();
+        }
+        return String.format("Type error at line %d, column %d: %s", position.startLine, position.startColumn,
+                super.getMessage());
+    }
+}
