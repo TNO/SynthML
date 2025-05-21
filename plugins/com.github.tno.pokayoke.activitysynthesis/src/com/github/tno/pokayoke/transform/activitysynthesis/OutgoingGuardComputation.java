f9cf7762cef179aac42fb2b410d68153253706cc
Wytse Oortwijn <wytse.oortwijn@tno.nl>
2025-05-21 11:35:27 +0200

    wip

--- plugins/com.github.tno.pokayoke.activitysynthesis/src/com/github/tno/pokayoke/transform/activitysynthesis/OutgoingGuardComputation.java
+++ plugins/com.github.tno.pokayoke.activitysynthesis/src/com/github/tno/pokayoke/transform/activitysynthesis/OutgoingGuardComputation.java
@@ -3,10 +3,13 @@ package com.github.tno.pokayoke.transform.activitysynthesis;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
 
 import org.eclipse.escet.cif.bdd.conversion.CifToBddConverter;
-import org.eclipse.escet.cif.bdd.conversion.CifToBddConverter.UnsupportedPredicateException;
 import org.eclipse.escet.cif.bdd.spec.CifBddEdge;
 import org.eclipse.escet.cif.bdd.spec.CifBddSpec;
 import org.eclipse.escet.cif.datasynth.settings.BddSimplify;
@@ -14,9 +17,8 @@ import org.eclipse.escet.cif.datasynth.settings.CifDataSynthesisSettings;
 import org.eclipse.escet.cif.datasynth.settings.FixedPointComputationsOrder;
 import org.eclipse.escet.cif.metamodel.cif.Specification;
 import org.eclipse.escet.cif.metamodel.cif.declarations.Event;
-import org.eclipse.escet.cif.metamodel.cif.expressions.Expression;
-import org.eclipse.escet.cif.metamodel.java.CifConstructors;
 import org.eclipse.uml2.uml.ActivityEdge;
+import org.eclipse.uml2.uml.ActivityFinalNode;
 import org.eclipse.uml2.uml.ActivityNode;
 import org.eclipse.uml2.uml.ControlFlow;
 import org.eclipse.uml2.uml.RedefinableElement;
@@ -25,6 +27,8 @@ import com.github.javabdd.BDD;
 import com.github.javabdd.BDDFactory;
 import com.github.javabdd.BDDVarSet;
 import com.github.tno.pokayoke.transform.uml2cif.UmlToCifTranslator;
+import com.github.tno.pokayoke.uml.profile.util.PokaYokeUmlProfileUtil;
+import com.google.common.base.Verify;
 
 public class OutgoingGuardComputation extends GuardComputation {
     private final UmlToCifTranslator translator;
@@ -49,7 +53,7 @@ public class OutgoingGuardComputation extends GuardComputation {
         // Convert the CIF specification to a CIF/BDD specification.
         CifToBddConverter.preprocess(specification, settings.getWarnOutput(), settings.getDoPlantsRefReqsWarn());
         BDDFactory factory = CifToBddConverter.createFactory(settings, new ArrayList<>(), new ArrayList<>());
-        CifToBddConverter converter = new CifToBddConverter("Outgoing guard computation");
+        CifToBddConverter converter = new CifToBddConverter("Guard computation");
         CifBddSpec cifBddSpec = converter.convert(specification, settings, factory);
 
         // Find all uncontrolled and controlled system states.
@@ -59,57 +63,100 @@ public class OutgoingGuardComputation extends GuardComputation {
         // Obtain the set of all internal BDD variables.
         BDDVarSet internalVars = getInternalBDDVars(cifBddSpec);
 
-        // TODO obtain CIF start event map.
-        Map<Event, RedefinableElement> startEventMap = translator.getStartEventMap();
+        // TODO obtain CIF event maps.
+        Map<RedefinableElement, List<Event>> startEventMap = reverse(translator.getStartEventMap());
+        Map<Event, List<Event>> nonDeterministicEventMap = translator.getNonDeterministicEvents();
+
+        // TODO helper function
+        Function<Event, CifBddEdge> getCorrespondingEdge = event -> {
+            List<CifBddEdge> edges = cifBddSpec.eventEdges.get(event);
+            Verify.verify(edges.size() == 1);
+            return edges.get(0);
+        };
+
+        // TODO what about intermediate/virtual control flows for non-atomic actions?
+
+        // TODO explain why guards can't become 'false'.
 
         // TODO compute outgoing guard for every control flow.
-        for (ActivityEdge edge: translator.getActivity().getEdges()) {
-            if (edge instanceof ControlFlow controlFlow) {
-                ActivityNode target = controlFlow.getTarget();
+        for (ActivityEdge controlFlow: translator.getActivity().getEdges()) {
+            ActivityNode target = controlFlow.getTarget();
 
-                System.out.println("Computing outgoing guard for control flow to: " + target);
+            // TODO not sure about this. We still remove a token then....
+            // We will not execute the activity final node.
+            if (target instanceof ActivityFinalNode) {
 
-                // TODO compute guard
-                BDD guardUncontrolled = cifBddSpec.factory.zero();
-                BDD guardControlled = cifBddSpec.factory.zero();
+                // TODO probably we must consider all controlled system states where there is a token on 'controlFlow'
+                // in which the activity postcondition holds. I.e., in which you are marked. From those states you
+                // can finish the activity. Same with uncontrolled states, and then simplify.
+                continue;
+            }
 
-                // TODO note that 'target' may have multiple CIF start events. We should consider them all.
-                for (CifBddEdge cifEdge: cifBddSpec.edges) {
-                    // TODO properly handle end events of atomic non-deteterministic actions.
+            System.out.println("Computing outgoing guard for control flow to: " + target);
 
-                    // What can 'cifEdge' be:
-                    // - The start+end event of an atomic deterministic action -> take edge (case 1)
-                    // - The start event of an atomic non-deterministic action -> don't take edge
-                    // - The start event of a non-atomic action -> take edge (case 1)
-                    // - The end event of an atomic non-deterministic action -> take start + end event  (case 2)
-                    // - The end event of a non-atomic action -> don't take edge
+            // TODO compute guard
+            BDD guardUncontrolled = cifBddSpec.factory.zero();
+            BDD guardControlled = cifBddSpec.factory.zero();
 
-                    // So:
-                    // - if 'cifEdge' is the start event of an atomic deterministic or non-atomic action, take the edge
-                    // - if 'cifEdge' is the end event of an atomic non-deterministic action, take both start+end edge
-                    // - otherwise, don't take the edge
+            // Apply 'target' backwards to find out in what states 'controlFlow' can lose its token in the controlled
+            // and the uncontrolled system. In case 'target' is atomic and non-deterministic, we need to do two backward
+            // steps: for the end+start event. Otherwise, we only need to so the start event.
 
-                    if (cifEdge.event.getControllable() && startEventMap.get(cifEdge.event).equals(target)) {
-                        guardUncontrolled = guardUncontrolled
-                                .orWith(applyBackward(cifEdge, uncontrolledStates.id(), uncontrolledStates));
-                        guardControlled = guardControlled
-                                .orWith(applyBackward(cifEdge, controlledStates.id(), controlledStates));
+            // TODO also callable for control nodes?
+            // If 'target' is atomic and non-deterministic, then we must consider both start and end edges.
+            if (PokaYokeUmlProfileUtil.isAtomic(target) && !PokaYokeUmlProfileUtil.isDeterministic(target)) {
+                for (Event startEvent: startEventMap.get(target)) {
+                    CifBddEdge startEdge = getCorrespondingEdge.apply(startEvent);
+
+                    for (Event endEvent: nonDeterministicEventMap.get(startEvent)) {
+                        CifBddEdge endEdge = getCorrespondingEdge.apply(endEvent);
+
+                        guardUncontrolled = guardUncontrolled.orWith(applyBackward(startEdge,
+                                applyBackward(endEdge, uncontrolledStates.id(), uncontrolledStates),
+                                uncontrolledStates));
+                        guardControlled = guardControlled.orWith(applyBackward(startEdge,
+                                applyBackward(endEdge, controlledStates.id(), controlledStates), controlledStates));
                     }
                 }
+            } else {
+                for (Event startEvent: startEventMap.get(target)) {
+                    CifBddEdge startEdge = getCorrespondingEdge.apply(startEvent);
 
-                // Try to compute an outgoing guard that is independent of internal variables.
-                BDD abstractControlled = guardControlled.exist(internalVars);
-                BDD abstractUncontrolled = guardUncontrolled.exist(internalVars);
-                BDD outgoingGuard = abstractControlled.simplify(abstractUncontrolled);
-                abstractControlled.free();
-                abstractUncontrolled.free();
-
-                System.out.println("Outgoing guard: " + bddToString(outgoingGuard, cifBddSpec));
-                System.out.println();
+                    guardUncontrolled = guardUncontrolled
+                            .orWith(applyBackward(startEdge, uncontrolledStates.id(), uncontrolledStates));
+                    guardControlled = guardControlled
+                            .orWith(applyBackward(startEdge, controlledStates.id(), controlledStates));
+                }
             }
+
+            // Try to compute an outgoing guard that is independent of internal variables.
+            BDD abstractControlled = guardControlled.exist(internalVars);
+            BDD abstractUncontrolled = guardUncontrolled.exist(internalVars);
+            BDD outgoingGuard = abstractControlled.simplify(abstractUncontrolled);
+            abstractControlled.free();
+            abstractUncontrolled.free();
+
+            // TODO two checks that the guard is correct.
+
+            // Free intermediate BDDs.
+            guardUncontrolled.free();
+            guardControlled.free();
+
+            System.out.println("Outgoing guard: " + bddToString(outgoingGuard, cifBddSpec));
+            System.out.println();
         }
 
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private static <T, U> Map<U, List<T>> reverse(Map<T, U> map) {
+        Map<U, List<T>> result = new LinkedHashMap<>();
+
+        for (Entry<T, U> entry: map.entrySet()) {
+            result.computeIfAbsent(entry.getValue(), e -> new ArrayList<>()).add(entry.getKey());
+        }
+
+        return result;
     }
 }
