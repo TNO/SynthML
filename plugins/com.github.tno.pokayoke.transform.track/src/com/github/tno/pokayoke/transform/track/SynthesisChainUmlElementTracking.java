3efb965e2775b766e427c7b0d8dcdc793e542790
AndreaPuffo <andrea.peruffo.05@gmail.com>
2025-07-23 09:46:13 +0200

    #543 Add `SynthesisChainUmlElementTracking` class.

---
+++ plugins/com.github.tno.pokayoke.transform.track/src/com/github/tno/pokayoke/transform/track/SynthesisChainUmlElementTracking.java
@@ -0,0 +1,13 @@
+
+package com.github.tno.pokayoke.transform.track;
+
+/**
+ * Tracks the synthesis chain transformations from the UML elements of the input model, to their translation to CIF
+ * events, the translation to Petri net transitions, and finally to the synthesized activity UML opaque actions (before
+ * model finalization).
+ */
+public class SynthesisChainUmlElementTracking {
+    public SynthesisChainUmlElementTracking() {
+        // Empty constructor.
+    }
+}
