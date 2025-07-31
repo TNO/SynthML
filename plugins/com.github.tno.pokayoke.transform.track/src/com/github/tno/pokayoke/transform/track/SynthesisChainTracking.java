a605d55fa4f62935868d2113d48b527f2b74f20f
AndreaPuffo <46629578+AndreaPuffo@users.noreply.github.com>
2025-07-31 10:52:02 +0200

    #543 Apply suggestions from code review
    Co-authored-by: Wytse Oortwijn <wytseoortwijn@gmail.com>

--- plugins/com.github.tno.pokayoke.transform.track/src/com/github/tno/pokayoke/transform/track/SynthesisChainTracking.java
+++ plugins/com.github.tno.pokayoke.transform.track/src/com/github/tno/pokayoke/transform/track/SynthesisChainTracking.java
@@ -10,19 +10,19 @@ import org.eclipse.escet.cif.metamodel.cif.declarations.Event;
 import org.eclipse.uml2.uml.RedefinableElement;
 
 /**
- * Tracks the synthesis chain transformations from the UML elements of the input model, to their translation to CIF
+ * Tracks the activity synthesis chain transformations from the UML elements of the input model, to their translation to CIF
  * events, the translation to Petri net transitions, to the synthesized activity UML opaque actions, to the UML to CIF
  * transformation needed for guard computation and the UML to CIF transformation for the language equivalence check.
  */
 public class SynthesisChainTracking {
     /**
-     * The map from CIF events generated for the synthesis to their corresponding UML elements. Gets updated as the
-     * synthesis chain rewrites, removes, or add events.
+     * The map from CIF events generated for the initial data-based synthesis to their corresponding UML elements of the input model. Gets updated as the
+     * activity synthesis chain rewrites, removes, or add events.
      */
-    private Map<Event, RedefinableElement> synthesisCifEventsToUmlElementInfo = new LinkedHashMap<>();
+    private final Map<Event, RedefinableElement> synthesisCifEventsToUmlElementInfo = new LinkedHashMap<>();
 
-    /** The set of internal CIF events (e.g. corresponding to control nodes) generated for synthesis. */
-    private Set<Event> internalSynthesisEvents = new LinkedHashSet<>();
+    /** The set of internal CIF events (e.g. corresponding to control nodes) generated for the initial data-based synthesis. */
+    private final Set<Event> internalSynthesisEvents = new LinkedHashSet<>();
 
     public SynthesisChainTracking() {
         // Empty constructor.
