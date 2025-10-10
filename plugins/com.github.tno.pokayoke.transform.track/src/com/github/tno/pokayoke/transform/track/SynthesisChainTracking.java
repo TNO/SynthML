732e1e9ea4f487ec8af95a8e6304d456b846e701
AndreaPuffo <andrea.peruffo.05@gmail.com>
2025-10-10 11:53:32 +0200

    #543 SynthesisChainTracking: update comments.

--- plugins/com.github.tno.pokayoke.transform.track/src/com/github/tno/pokayoke/transform/track/SynthesisChainTracking.java
+++ plugins/com.github.tno.pokayoke.transform.track/src/com/github/tno/pokayoke/transform/track/SynthesisChainTracking.java
@@ -638,7 +638,7 @@ public class SynthesisChainTracking {
 
         /**
          * Indicates whether the transition relates to a CIF event that is both a start and end event (e.g. related to
-         * an atomic behavior) or if the transition relates to a merged (rewritten) non-atomic pattern.
+         * an atomic opaque behavior) or if the transition relates to a merged (rewritten) non-atomic pattern.
          *
          * @return {@code true} if the transition is merged or is related to a start and end CIF event, {@code false}
          *     otherwise.
@@ -975,12 +975,12 @@ public class SynthesisChainTracking {
         RedefinableElement finalizedUmlElement = finalizedEventInfo.getUmlElement();
         Verify.verify(isFinalizedUmlElement(finalizedUmlElement),
                 String.format("Element '%s' is not a finalized UML element.", finalizedUmlElement.getName()));
-        // This handles three cases: when a non-atomic pattern is not merged, it can link back to the original CIF event
+        // This handles three cases: when a non-atomic pattern is not merged, it links back to the original CIF event
         // and check that it is a start-only event; when a non-atomic pattern is merged, it links to an original
-        // complete CIF event while the current CIF event is a start-only event; an atomic behavior, that results in
-        // complete original CIF event and a complete current CIF event. These last two cases can be combined, checking
-        // that the original CIF event is complete and the current CIF event is a start event (i.e. start-only or
-        // complete).
+        // complete CIF event while the current CIF event is a start-only event; an atomic opaque behavior, that results
+        // in complete original CIF event and a complete current CIF event. These last two cases can be combined,
+        // checking that the original CIF event is complete and the current CIF event is a start event (i.e. start-only
+        // or complete).
         return isRelatedToOriginalStartOnlyEvent(finalizedUmlElement)
                 || (isRelatedToOriginalCompleteEvent(finalizedUmlElement) && finalizedEventInfo.isStartEvent());
     }
@@ -998,7 +998,7 @@ public class SynthesisChainTracking {
         RedefinableElement finalizedUmlElement = finalizedEventInfo.getUmlElement();
         Verify.verify(isFinalizedUmlElement(finalizedUmlElement),
                 String.format("Element '%s' is not a finalized UML element.", finalizedUmlElement.getName()));
-        // This handles two cases: when a non-atomic pattern is not merged, it can link back to the original CIF event
+        // This handles two cases: when a non-atomic pattern is not merged, it links back to the original CIF event
         // and check that it is an end-only event; when a non-atomic pattern is merged, it links to an original
         // complete CIF event while the current CIF event is an end-only event.
         return isRelatedToOriginalEndOnlyEvent(finalizedUmlElement)
