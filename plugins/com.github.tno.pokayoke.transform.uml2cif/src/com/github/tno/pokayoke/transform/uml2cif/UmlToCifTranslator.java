a99b7dd602194ef0d09242608ad67f40869cf289
AndreaPuffo <andrea.peruffo.05@gmail.com>
2025-10-10 16:54:15 +0200

    #543 UmlToCifTranslator: reformat file.

--- plugins/com.github.tno.pokayoke.transform.uml2cif/src/com/github/tno/pokayoke/transform/uml2cif/UmlToCifTranslator.java
+++ plugins/com.github.tno.pokayoke.transform.uml2cif/src/com/github/tno/pokayoke/transform/uml2cif/UmlToCifTranslator.java
@@ -1610,7 +1610,8 @@ public class UmlToCifTranslator extends ModelToCifTranslator {
                         yield PostConditionKind.WITHOUT_STRUCTURE;
                     } else {
                         // We must allow finishing non-atomic/non-deterministic actions.
-                        Verify.verify(synthesisTracker.isRelatedToEndOnlyOfOriginalElement(cifEvent, translationPurpose),
+                        Verify.verify(
+                                synthesisTracker.isRelatedToEndOnlyOfOriginalElement(cifEvent, translationPurpose),
                                 "Event '%s' is not an end-only event.", cifEvent.getName());
 
                         yield PostConditionKind.WITH_STRUCTURE;
