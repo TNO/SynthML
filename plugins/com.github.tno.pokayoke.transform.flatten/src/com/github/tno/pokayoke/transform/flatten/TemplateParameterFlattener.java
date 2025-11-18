0ce4df278c894bffeda9d0231abf823d8d13de41
darets97 <dennis.arets@vdletg.com>
2025-11-18 16:59:00 +0100

    #635 Rename argumentMap to nameToArgument

--- plugins/com.github.tno.pokayoke.transform.flatten/src/com/github/tno/pokayoke/transform/flatten/TemplateParameterFlattener.java
+++ plugins/com.github.tno.pokayoke.transform.flatten/src/com/github/tno/pokayoke/transform/flatten/TemplateParameterFlattener.java
@@ -43,13 +43,13 @@ public class TemplateParameterFlattener {
      * Unfolds guards and effects of a redefinable element.
      *
      * @param element The redefinable element.
-     * @param argumentMap The mapping of template parameter names to concrete arguments.
+     * @param nameToArgument The mapping of template parameter names to concrete arguments.
      */
-    private static void unfoldRedefinableElement(RedefinableElement element, Map<String, AExpression> argumentMap) {
+    private static void unfoldRedefinableElement(RedefinableElement element, Map<String, AExpression> nameToArgument) {
         // Perform the guard unfolding. Skip if there is no guard.
         AExpression guardExpr = CifParserHelper.parseGuard(element);
         if (guardExpr != null) {
-            AExpression newGuard = unfoldAExpression(guardExpr, argumentMap);
+            AExpression newGuard = unfoldAExpression(guardExpr, nameToArgument);
             String newGuardString = ACifObjectToString.toString(newGuard);
             PokaYokeUmlProfileUtil.setGuard(element, newGuardString);
         }
@@ -59,7 +59,7 @@ public class TemplateParameterFlattener {
         List<String> newEffects = new ArrayList<>();
         for (String effect: effects) {
             List<AUpdate> updates = CifParserHelper.parseUpdates(effect, element);
-            String newEffect = updates.stream().flatMap(update -> unfoldAUpdate(update, argumentMap).stream())
+            String newEffect = updates.stream().flatMap(update -> unfoldAUpdate(update, nameToArgument).stream())
                     .map(newUpdate -> ACifObjectToString.toString(newUpdate)).collect(Collectors.joining(", "));
             newEffects.add(newEffect);
         }
@@ -70,32 +70,32 @@ public class TemplateParameterFlattener {
      * Unfolds a CIF {@link AExpression}: replaces template parameters with the given arguments.
      *
      * @param expression A CIF {@link AExpression} to be unfolded.
-     * @param argumentMap The mapping of template parameter names to concrete arguments.
+     * @param nameToArgument The mapping of template parameter names to concrete arguments.
      * @return The unfolded CIF {@link AExpression}.
      */
-    private static AExpression unfoldAExpression(AExpression expression, Map<String, AExpression> argumentMap) {
+    private static AExpression unfoldAExpression(AExpression expression, Map<String, AExpression> nameToArgument) {
         if (expression instanceof ABinaryExpression binExpr) {
-            AExpression unfoldedLhsExpression = unfoldAExpression(binExpr.left, argumentMap);
-            AExpression unfoldedRhsExpression = unfoldAExpression(binExpr.right, argumentMap);
+            AExpression unfoldedLhsExpression = unfoldAExpression(binExpr.left, nameToArgument);
+            AExpression unfoldedRhsExpression = unfoldAExpression(binExpr.right, nameToArgument);
 
             // Combine the unfolded left and right components to form a new binary expression.
             return new ABinaryExpression(binExpr.operator, unfoldedLhsExpression, unfoldedRhsExpression,
                     expression.position);
         } else if (expression instanceof AUnaryExpression unaryExpr) {
-            return new AUnaryExpression(unaryExpr.operator, unfoldAExpression(unaryExpr.child, argumentMap),
+            return new AUnaryExpression(unaryExpr.operator, unfoldAExpression(unaryExpr.child, nameToArgument),
                     unaryExpr.position);
         } else if (expression instanceof ANameExpression nameExpr) {
-            return argumentMap.getOrDefault(nameExpr.name.name, nameExpr);
+            return nameToArgument.getOrDefault(nameExpr.name.name, nameExpr);
         } else if (expression instanceof ABoolExpression || expression instanceof AIntExpression) {
             // Expressions without children don't need unfolding.
             return expression;
         } else if (expression instanceof AIfExpression ifExpr) {
-            List<AExpression> guardExprs = ifExpr.guards.stream().map(guard -> unfoldAExpression(guard, argumentMap))
+            List<AExpression> guardExprs = ifExpr.guards.stream().map(guard -> unfoldAExpression(guard, nameToArgument))
                     .toList();
-            AExpression thenExpr = unfoldAExpression(ifExpr.then, argumentMap);
+            AExpression thenExpr = unfoldAExpression(ifExpr.then, nameToArgument);
             List<AElifExpression> elifExprs = ifExpr.elifs.stream()
-                    .map(elif -> unfoldAElifExpression(elif, argumentMap)).toList();
-            AExpression elseExpr = unfoldAExpression(ifExpr.elseExpr, argumentMap);
+                    .map(elif -> unfoldAElifExpression(elif, nameToArgument)).toList();
+            AExpression elseExpr = unfoldAExpression(ifExpr.elseExpr, nameToArgument);
             return new AIfExpression(guardExprs, thenExpr, elifExprs, elseExpr, ifExpr.position);
         } else {
             throw new RuntimeException(String.format("Unfolding expressions of class '%s' is not supported.",
@@ -104,11 +104,11 @@ public class TemplateParameterFlattener {
     }
 
     private static AElifExpression unfoldAElifExpression(AElifExpression elifExpr,
-            Map<String, AExpression> argumentMap)
+            Map<String, AExpression> nameToArgument)
     {
-        List<AExpression> guardExprs = elifExpr.guards.stream().map(guard -> unfoldAExpression(guard, argumentMap))
+        List<AExpression> guardExprs = elifExpr.guards.stream().map(guard -> unfoldAExpression(guard, nameToArgument))
                 .toList();
-        AExpression thenExpr = unfoldAExpression(elifExpr.then, argumentMap);
+        AExpression thenExpr = unfoldAExpression(elifExpr.then, nameToArgument);
         return new AElifExpression(guardExprs, thenExpr, elifExpr.position);
     }
 
@@ -116,14 +116,14 @@ public class TemplateParameterFlattener {
      * Unfolds a CIF {@link AUpdate}: Replaces template parameters with the given arguments.
      *
      * @param update A CIF {@link AUpdate} to be unfolded.
-     * @param argumentMap The mapping of template parameter names to concrete arguments.
+     * @param nameToArgument The mapping of template parameter names to concrete arguments.
      * @return The list containing the unfolded CIF {@link AUpdate}.
      */
-    private static List<AUpdate> unfoldAUpdate(AUpdate update, Map<String, AExpression> argumentMap) {
+    private static List<AUpdate> unfoldAUpdate(AUpdate update, Map<String, AExpression> nameToArgument) {
         if (update instanceof AAssignmentUpdate assign) {
-            return unfoldAAssignmentUpdate(assign, argumentMap);
+            return unfoldAAssignmentUpdate(assign, nameToArgument);
         } else if (update instanceof AIfUpdate ifUpdate) {
-            AUpdate newIfUpdate = unfoldAIfUpdate(ifUpdate, argumentMap);
+            AUpdate newIfUpdate = unfoldAIfUpdate(ifUpdate, nameToArgument);
             return List.of(newIfUpdate);
         } else {
             throw new RuntimeException(
@@ -135,61 +135,62 @@ public class TemplateParameterFlattener {
      * Unfolds a CIF {@link AAssignmentUpdate}: Replaces template parameters with the given arguments.
      *
      * @param assignUpdate A CIF {@link AAssignmentUpdate} to be unfolded.
-     * @param argumentMap The mapping of template parameter names to concrete arguments.
+     * @param nameToArgument The mapping of template parameter names to concrete arguments.
      * @return The unfolded CIF {@link AAssignmentUpdate}.
      */
     private static List<AUpdate> unfoldAAssignmentUpdate(AAssignmentUpdate assignUpdate,
-            Map<String, AExpression> argumentMap)
+            Map<String, AExpression> nameToArgument)
     {
         // Sanity check: 'addressable' must be a name expression which must not be a parameter.
         Verify.verify(assignUpdate.addressable instanceof ANameExpression adressable
-                && !argumentMap.containsKey(adressable.name.name));
+                && !nameToArgument.containsKey(adressable.name.name));
 
         return List.of(new AAssignmentUpdate(assignUpdate.addressable,
-                unfoldAExpression(assignUpdate.value, argumentMap), assignUpdate.position));
+                unfoldAExpression(assignUpdate.value, nameToArgument), assignUpdate.position));
     }
 
     /**
      * Unfolds a CIF {@link AIfUpdate}: Replaces template parameters with the given arguments.
      *
      * @param ifUpdate A CIF {@link AIfUpdate} to be unfolded.
-     * @param argumentMap The mapping of template parameter names to concrete arguments.
+     * @param nameToArgument The mapping of template parameter names to concrete arguments.
      * @return The unfolded CIF {@link AIfUpdate}.
      */
-    private static AUpdate unfoldAIfUpdate(AIfUpdate ifUpdate, Map<String, AExpression> argumentMap) {
+    private static AUpdate unfoldAIfUpdate(AIfUpdate ifUpdate, Map<String, AExpression> nameToArgument) {
         // Process the 'if' statements.
-        List<AExpression> unfoldedGuards = ifUpdate.guards.stream().map(u -> unfoldAExpression(u, argumentMap))
+        List<AExpression> unfoldedGuards = ifUpdate.guards.stream().map(u -> unfoldAExpression(u, nameToArgument))
                 .toList();
 
         // Process the 'then' statements.
-        List<AUpdate> unfoldedThens = ifUpdate.thens.stream().flatMap(u -> unfoldAUpdate(u, argumentMap).stream())
+        List<AUpdate> unfoldedThens = ifUpdate.thens.stream().flatMap(u -> unfoldAUpdate(u, nameToArgument).stream())
                 .toList();
 
         // Process the 'elif' statements.
-        List<AElifUpdate> unfoldedElifs = ifUpdate.elifs.stream().map(u -> unfoldAElifUpdate(u, argumentMap)).toList();
+        List<AElifUpdate> unfoldedElifs = ifUpdate.elifs.stream().map(u -> unfoldAElifUpdate(u, nameToArgument))
+                .toList();
 
         // Process the 'else' statements.
-        List<AUpdate> unfoldedElses = ifUpdate.elses.stream().flatMap(u -> unfoldAUpdate(u, argumentMap).stream())
+        List<AUpdate> unfoldedElses = ifUpdate.elses.stream().flatMap(u -> unfoldAUpdate(u, nameToArgument).stream())
                 .toList();
 
         return new AIfUpdate(unfoldedGuards, unfoldedThens, unfoldedElifs, unfoldedElses, ifUpdate.position);
     }
 
-    private static AElifUpdate unfoldAElifUpdate(AElifUpdate elifUpdate, Map<String, AExpression> argumentMap) {
+    private static AElifUpdate unfoldAElifUpdate(AElifUpdate elifUpdate, Map<String, AExpression> nameToArgument) {
         // Process the 'guards'.
-        List<AExpression> unfoldedElifGuards = elifUpdate.guards.stream().map(u -> unfoldAExpression(u, argumentMap))
+        List<AExpression> unfoldedElifGuards = elifUpdate.guards.stream().map(u -> unfoldAExpression(u, nameToArgument))
                 .toList();
 
         // Process the 'thens'.
-        List<AUpdate> unfoldedElifThens = elifUpdate.thens.stream().flatMap(u -> unfoldAUpdate(u, argumentMap).stream())
-                .toList();
+        List<AUpdate> unfoldedElifThens = elifUpdate.thens.stream()
+                .flatMap(u -> unfoldAUpdate(u, nameToArgument).stream()).toList();
 
         return new AElifUpdate(unfoldedElifGuards, unfoldedElifThens, elifUpdate.position);
     }
 
     public static void unfoldActivity(Activity activity, CallBehaviorAction callBehaviorActionToReplace) {
         List<AAssignmentUpdate> activityArguments = CifParserHelper.parseArguments(callBehaviorActionToReplace);
-        Map<String, AExpression> argumentMap = activityArguments.stream().collect(
+        Map<String, AExpression> nameToArgument = activityArguments.stream().collect(
                 Collectors.toMap(update -> ((ANameExpression)update.addressable).name.name, update -> update.value));
 
         // Unfold the guards and effects of owned elements.
@@ -198,22 +199,22 @@ public class TemplateParameterFlattener {
                 // Get the incoming and outgoing guards, unfold them, and substitute the corresponding string.
                 AExpression incomingGuard = CifParserHelper.parseIncomingGuard(controlEdge);
                 if (incomingGuard != null) {
-                    AExpression unfoldedIncoming = unfoldAExpression(incomingGuard, argumentMap);
+                    AExpression unfoldedIncoming = unfoldAExpression(incomingGuard, nameToArgument);
                     PokaYokeUmlProfileUtil.setIncomingGuard(controlEdge, ACifObjectToString.toString(unfoldedIncoming));
                 }
 
                 AExpression outgoingGuard = CifParserHelper.parseOutgoingGuard(controlEdge);
                 if (outgoingGuard != null) {
-                    AExpression unfoldedOutgoing = unfoldAExpression(outgoingGuard, argumentMap);
+                    AExpression unfoldedOutgoing = unfoldAExpression(outgoingGuard, nameToArgument);
                     PokaYokeUmlProfileUtil.setOutgoingGuard(controlEdge, ACifObjectToString.toString(unfoldedOutgoing));
                 }
             } else if (ownedElement instanceof CallBehaviorAction callBehavior) {
                 if (PokaYokeUmlProfileUtil.isFormalElement(callBehavior)) {
                     // Shadowed call, process guards and effects of the call behavior.
-                    unfoldRedefinableElement(callBehavior, argumentMap);
+                    unfoldRedefinableElement(callBehavior, nameToArgument);
                 }
             } else if (ownedElement instanceof OpaqueAction internalAction) {
-                unfoldRedefinableElement(internalAction, argumentMap);
+                unfoldRedefinableElement(internalAction, nameToArgument);
             } else if (ownedElement instanceof Constraint) {
                 // Constraints cannot be parameterized.
                 continue;
