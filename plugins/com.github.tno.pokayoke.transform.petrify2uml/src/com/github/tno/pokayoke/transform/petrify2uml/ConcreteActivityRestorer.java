ded734b7b8fcc5e393bbe7b15abdcdc53fec3e89
AndreaPuffo <andrea.peruffo.05@gmail.com>
2025-11-25 12:06:15 +0100

    #630 ConcreteActivityRestorer: - create `restore` method - create `restoreConcreteDecisionMergeNodes` method

--- plugins/com.github.tno.pokayoke.transform.petrify2uml/src/com/github/tno/pokayoke/transform/petrify2uml/ConcreteActivityRestorer.java
+++ plugins/com.github.tno.pokayoke.transform.petrify2uml/src/com/github/tno/pokayoke/transform/petrify2uml/ConcreteActivityRestorer.java
@@ -1,8 +1,13 @@
 
 package com.github.tno.pokayoke.transform.petrify2uml;
 
+import java.util.Map.Entry;
+import java.util.Set;
+
 import org.eclipse.uml2.uml.Activity;
 import org.eclipse.uml2.uml.ActivityNode;
+import org.eclipse.uml2.uml.DecisionNode;
+import org.eclipse.uml2.uml.MergeNode;
 import org.eclipse.uml2.uml.RedefinableElement;
 
 import com.github.tno.pokayoke.transform.common.NameHelper;
@@ -25,8 +30,17 @@ public class ConcreteActivityRestorer {
         this.tracker = tracker;
     }
 
+    /**
+     * Restores the control flow guards from called concrete activities, and the decision (merge) node patterns deriving
+     * from decision (merge) nodes located in called concrete activities..
+     */
+    public void restore() {
+        restoreConcreteControlFlowGuards();
+        restoreConcreteDecisionMergeNodes();
+    }
+
     /** Restores the control flow guards from called concrete activities. */
-    public void restoreConcreteControlFlowGuards() {
+    private void restoreConcreteControlFlowGuards() {
         for (ActivityNode node: activity.getNodes()) {
             // Track the original UML element related to the current node. If the original UML element is 'null', or if
             // the original UML element belongs to the activity (e.g., the current node is a call behavior and the
@@ -52,5 +66,33 @@ public class ConcreteActivityRestorer {
             node.getIncomings().stream().forEach(e -> PokaYokeUmlProfileUtil.setOutgoingGuard(e, entryGuard));
             node.getOutgoings().stream().forEach(e -> PokaYokeUmlProfileUtil.setIncomingGuard(e, exitGuard));
         }
+    }
+
+    /**
+     * Restore the patterns of decision (and merge) nodes when: 1) All nodes can be traced back to the same decision
+     * (merge) node in a called concrete activity; 2) Each node has exactly one outgoing (incoming) edge; 3) That edge's
+     * source (target) is the new decision (merge) node created as a translation of a Petri net place with multiple
+     * outgoing (incoming) arcs.
+     */
+    private void restoreConcreteDecisionMergeNodes() {
+        for (Entry<ActivityNode, Set<ActivityNode>> pattern: tracker.getDecisionOrMergePatternNodes().entrySet()) {
+            ActivityNode newNode = pattern.getKey();
+            if (newNode instanceof DecisionNode decisionNode) {
+                restoreConcreteDecisionNodePattern(decisionNode, pattern.getValue());
+            } else if (newNode instanceof MergeNode mergeNode) {
+                restoreConcreteMergeNodePattern(mergeNode, pattern.getValue());
+            } else {
+                throw new RuntimeException(
+                        String.format("Node '%s' must be either a decision or a merge node.", newNode.getName()));
+            }
+        }
+    }
+
+    private void restoreConcreteDecisionNodePattern(DecisionNode decisionNode, Set<ActivityNode> childrenNodes) {
+        // TODO
+    }
+
+    private void restoreConcreteMergeNodePattern(MergeNode mergeNode, Set<ActivityNode> parentNodes) {
+        // TODO
     }
 }
