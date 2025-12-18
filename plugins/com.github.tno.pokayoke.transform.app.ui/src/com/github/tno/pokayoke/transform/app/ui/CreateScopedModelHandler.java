a0064658e5a8e62d004d327871be5ab83574d0a9
Wytse Oortwijn <wytse.oortwijn@tno.nl>
2025-12-18 17:29:39 +0100

    #655 Added license headers to all Java files

--- plugins/com.github.tno.pokayoke.transform.app.ui/src/com/github/tno/pokayoke/transform/app/ui/CreateScopedModelHandler.java
+++ plugins/com.github.tno.pokayoke.transform.app.ui/src/com/github/tno/pokayoke/transform/app/ui/CreateScopedModelHandler.java
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023-2025 TNO and Contributors to the GitHub community
+//
+// This program and the accompanying materials are made available under the terms of the
+// Eclipse Public License v2.0 which accompanies this distribution, and is available at
+// https://spdx.org/licenses/EPL-2.0.html
+//
+// SPDX-License-Identifier: EPL-2.0
+////////////////////////////////////////////////////////////////////////////////////////
 
 package com.github.tno.pokayoke.transform.app.ui;
 
