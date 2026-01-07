f8897663defdc4569233ef9fa3f74b6665aecbef
Wytse Oortwijn <wytse.oortwijn@tno.nl>
2026-01-07 08:13:20 +0100

    #676 Update license headers

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ For developers:
 
 ## License
 
-Copyright (c) 2023-2025 TNO and Contributors to the GitHub community
+Copyright (c) 2023-2026 TNO and Contributors to the GitHub community
 
 This program and the accompanying materials are made available under the terms of the [Eclipse Public License v2.0](https://spdx.org/licenses/EPL-2.0.html).
 
--- plugins/com.github.tno.synthml.uml.profile.util/src/com/github/tno/synthml/uml/profile/cif/TypeException.java
+++ plugins/com.github.tno.synthml.uml.profile.util/src/com/github/tno/synthml/uml/profile/cif/TypeException.java
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2023-2025 TNO and Contributors to the GitHub community
+// Copyright (c) 2023-2026 TNO and Contributors to the GitHub community
 //
 // This program and the accompanying materials are made available under the terms of the
 // Eclipse Public License v2.0 which accompanies this distribution, and is available at
